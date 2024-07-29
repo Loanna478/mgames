21c475314e0403b3ed0f4fd7786527ab83948a7e
Loen48 <MaskeZenDash@proton.me>
2024-07-29 04:11:19 +0200

    jkh

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,120 @@
-import Image from "next/image";
+"use client"
+import { useState } from 'react';
+import Image from 'next/image';
+import { games } from '../lib/data';
 
 export default function Home() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [filter, setFilter] = useState<'all' | 'solo' | 'multi'>('all');
+  const gamesPerPage = 60;
+
+  // Logic for determining if a game is solo or multi
+  const processedGames = games.map(game => ({
+    ...game,
+    type: game.multiImage === "https://i.imgur.com/QqcacDN.png" || game.multiImage === "https://i.imgur.com/4gWV1BJ.png" ? 'multi' : 'solo'
+  }));
+
+  // Logic for filtering games
+  const filteredGames = processedGames.filter(game => {
+    if (filter === 'solo') {
+      return game.type === 'solo';
+    } else if (filter === 'multi') {
+      return game.type === 'multi';
+    } else {
+      return true; // Return all games if no filter is applied
+    }
+  });
+
+  // Logic for displaying current games
+  const indexOfLastGame = currentPage * gamesPerPage;
+  const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+  const currentGames = filteredGames.slice(indexOfFirstGame, indexOfLastGame);
+
+  // Logic for displaying page numbers
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(filteredGames.length / gamesPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  const handleFilterChange = (filter: 'all' | 'solo' | 'multi') => {
+    setFilter(filter);
+    setCurrentPage(1);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-slate-900">
+      <nav className="fixed top-3 left-1/2 transform -translate-x-1/2 bg-black/50 backdrop-blur-md text-white z-50 py-4 px-6 md:px-12 flex justify-between items-center rounded-xl w-full md:w-3/4 max-w-5xl">
+        <a href="/" className="text-xl font-bold">M-GAMES</a>
+        <div className="hidden md:flex space-x-6 items-center">
+          <button onClick={() => handleFilterChange('solo')} className="hover:text-blue-400">Jeux Solo</button>
+          <button onClick={() => handleFilterChange('multi')} className="hover:text-blue-400">Jeux Multijoueur</button>
+          <button onClick={() => handleFilterChange('all')} className="hover:text-blue-400">Tous les Jeux</button>
+        </div>
+      </nav>
+
+      <header className="w-full py-20">
+        <div className="container mx-auto text-center">
+          <h1 className="text-4xl md:text-5xl font-extrabold text-white">
+            MGames
+          </h1>
+          <p className="mt-2 text-lg">Discover and download your favorite games</p>
+        </div>
+      </header>
+
+      <div className="container mx-auto py-8 px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+          {currentGames.map((game, index) => (
+            <a
+              key={index}
+              href={game.downloadLink}
+              target="_blank" // Ouvrir dans une nouvelle page
+              className="relative flex items-center group cursor-pointer overflow-hidden rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105 bg-slate-800"
+            >
+              <Image
+                src={game.image}
+                alt={game.name}
+                width={120}  // Ajuste la largeur
+                height={80} // Ajuste la hauteur
+                className="object-cover rounded-lg custom-image"
+                quality={100} // Réduit la qualité de l'image pour améliorer les performances
+              />
+              <div className="ml-4 flex-grow">
+                <h2 className="text-lg font-semibold text-white">{game.name}</h2>
+              </div>
+              <div className="absolute bottom-0 left-0 mb-2 ml-2">
+                <Image
+                  src={game.type === 'solo' ? '/solo.svg' : '/multi.svg'}
+                  alt={game.type === 'solo' ? 'Jeu Solo' : 'Jeu Multijoueur'}
+                  width={30}  // Ajuste la taille du logo
+                  height={30} // Ajuste la taille du logo
+                  className='bg-slate-700 rounded-lg'
+                />
+              </div>
+              <div className="absolute bottom-0 right-0 mb-2 mr-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <span className="px-2 py-1 bg-black text-white text-xs rounded">
+                  {game.type === 'solo' ? 'Jeu Solo' : 'Jeu Multijoueur'}
+                </span>
+              </div>
+            </a>
+          ))}
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer className="w-full bg-slate-900 py-6">
+        <div className="container mx-auto flex justify-center space-x-4">
+          {pageNumbers.map(number => (
+            <button
+              key={number}
+              onClick={() => paginate(number)}
+              className={`px-3 py-1 rounded-lg transition-colors duration-300 ${number === currentPage ? 'bg-blue-500 text-white' : 'bg-gray-600 text-gray-300 hover:bg-blue-400'}`}
+            >
+              {number}
+            </button>
+          ))}
+        </div>
+      </footer>
     </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'i.imgur.com',
+          pathname: '/**',
+        },
 
-export default nextConfig;
+        // Add more patterns as needed
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
